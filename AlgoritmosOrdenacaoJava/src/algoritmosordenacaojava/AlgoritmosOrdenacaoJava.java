7b52fc810e4a6aae9375c0ece25649c2dcf843af
marcel <alanmarcel2@gmail.com>
2020-02-12 16:24:08 -0300

    Teste de commit

--- AlgoritmosOrdenacaoJava/src/algoritmosordenacaojava/AlgoritmosOrdenacaoJava.java
+++ AlgoritmosOrdenacaoJava/src/algoritmosordenacaojava/AlgoritmosOrdenacaoJava.java
@@ -8,7 +8,7 @@ package algoritmosordenacaojava;
 public class AlgoritmosOrdenacaoJava {
 
     public static void main(String[] args) {
-        
+        System.out.println("Aqui");
         
     }
     
